97637921a87bb5ab9985b613ed4e2d1369ab355a
Tucker Kline <tucker.kline@gmail.com>
2015-09-21 17:50:48 -0600

    delete works

--- javascript.js
+++ javascript.js
@@ -13,14 +13,17 @@ var mainControllerFunc = function($scope) {
 		$scope.author = ""
 	}
 
-	$scope.rate = function() {
-		var ratings = []
-		ratings.push($scope.rating)
-		console.log(ratings)
+	$scope.delete = function(index) {
+		$scope.quotes.splice(index, 1)
 	}
+		
 
 
 
 }
 
 angular.module('myApp').controller('mainController', ['$scope', mainControllerFunc])
+
+// User can click on the author of a quote to go to a separate page/sceeen that shows all quotes from that person.
+// Make a 'Random Quote' button that selects a quote at random and displays it in a popup (not an alert).
+// The user should be given the option to undo their last action.
